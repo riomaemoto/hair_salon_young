1af6f14287a607087fa5e4eb5ba1d56ec50480c9
Leo A <>
2022-10-17 00:14:13 +0900

    center logo

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 $(function () {
   $(".hamburger").on("click", function () {
     $(".listContent").slideToggle(500)
+    $(".line1").toggleClass("open")
+    $(".line2").toggleClass("open")
+    $(".line3").toggleClass("open")
   })
 })
+
+
