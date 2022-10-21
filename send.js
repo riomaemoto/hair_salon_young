7d73bac47bf795dc9211fcd4d89a6cc4add9c4c2
Leo A <>
2022-10-22 01:35:11 +0900

    contact

--- send.js
+++ send.js
@@ -1,24 +1,22 @@
+const element = document.getElementById("#pushTheButton");
 emailjs.init("iXD6JCsR5Tqlncl");
-const onClick = () => {
-  const element = document.querySelector(".pushTheButton");
 
-  // send mail
-  element.addEventListener("click", () => {
-    const inputName = document.querySelector("#inputName").value;
-    const inputEmail = document.querySelector("#inputEmail").value;
-    const inputContents = document.querySelector("#inputContents").value;
+// send mail
+element.addEventListener("click", () => {
+  console.log("success");
+  const inputName = document.querySelector("#inputName").value;
+  const inputEmail = document.querySelector("#inputEmail").value;
+  const inputContents = document.querySelector("#inputContents").value;
 
-    const templateVariables = {
-      email: inputEmail,
-      to_name: "leo",
-      from_name: inputName,
-      contents: inputContents,
-    };
-    emailjs
-      .send("service_ni3z47a", "template_qt15ukd", templateVariables)
-      .then(() => console.log("success"));
-  });
-};
+  const templateVariables = {
+    email: inputEmail,
+    to_name: "leo",
+    from_name: inputName,
+    contents: inputContents,
+  };
+  emailjs
+    .send("service_ni3z47a", "template_qt15ukd", templateVariables)
+    .then(() => console.log("success"));
+});
 
-onClick();
 
